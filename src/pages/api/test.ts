db35010b17743b48d5fc8b2f22e08456dacaa37c
Shubham Bhardwaj <shubhamb1810@gmail.com>
2020-06-28 00:43:08 -0400

    added utils to path

--- src/pages/api/test.ts
+++ src/pages/api/test.ts
@@ -1,7 +1,7 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { Firestore } from "@google-cloud/firestore";
 
-import {firestore} from '../../utils/FirestoreInit'
+import {firestore} from '@utils/FirestoreInit'
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
 	await firestore.collection("test").add({ test: 1 });
